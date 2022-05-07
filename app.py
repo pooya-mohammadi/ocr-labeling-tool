e25bf5de8ed3cea16c2c5c8720813c1231beb710
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-05-07 12:25:15 +0430

    update app

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+import shutil
+
 from flask import Flask, render_template, send_from_directory, request, redirect, url_for
 import os
 from PIL import Image
 from cursor import Cursor
 from desktopify import launch_gui
+from settings import CURSOR_FILE, DATA_FOLDER, TEXT_MAX_LEN, RESULTS_FOLDER, PORT
+
+os.makedirs(RESULTS_FOLDER, exist_ok=True)
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-# define app common configs
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['CURSOR_FILE'] = './cursor.json'
-app.config['DATA_FOLDER'] = './data'
-app.config['RESULTS_FOLDER'] = './results'
-app.config['TEXT_MAX_LEN'] = 24
 # fetch cursor file
-cursor = Cursor(path=app.config['CURSOR_FILE'], app=app)
+cursor = Cursor(path=CURSOR_FILE, data_dir=DATA_FOLDER, cursor_path=CURSOR_FILE)
 
 
 # app routes
@@ -22,41 +24,41 @@ cursor = Cursor(path=app.config['CURSOR_FILE'], app=app)
 def index():
     cursor.reload_file()
     text = os.path.splitext(cursor['images'][str(cursor['file_index_to_read'])])[0]
-    return render_template('index.html', text=text)
+    text = text.split("_")[0]
+    text_01 = text[:2]
+    text_02 = text[2]
+    text_03 = text[3:]
+    # photo = os.path.join(DATA_FOLDER, cursor['images'][str(cursor['file_index_to_read'])])
+    return render_template('index.html', text_01=text_01, text_02=text_02, text_03=text_03, text=text)
 
 
 @app.route('/get_next_image', methods=['GET'])
 def get_next_image():
     index = cursor['file_index_to_read']
-    if index >= len(cursor):
-        index = len(cursor)
     image_file = cursor['images'][str(index)]
-    # sends 'image_file' from app.config['DATA_FOLDER'] directory
-    return send_from_directory(app.config["DATA_FOLDER"], filename=image_file, cache_timeout=-1)
+    return send_from_directory(DATA_FOLDER, path=image_file, max_age=-1)
 
 
 @app.route('/action', methods=['POST', 'GET'])
 def action():
     if request.method == 'POST':
         if request.form['action'] == "Save":
-            text = request.form.get('text')
-            if len(text) <= app.config['TEXT_MAX_LEN']:
+            text = request.form.get('text_01') + request.form.get("text_02") + request.form.get("text_03")
+            if len(text) <= TEXT_MAX_LEN:
                 index = cursor['file_index_to_read']
-                img_path = os.path.join(app.config['DATA_FOLDER'], cursor['images'][str(index)])
+                img_path = os.path.join(DATA_FOLDER, cursor['images'][str(index)])
                 # get file extension e.g. jpg, png
                 img_extension = os.path.splitext(img_path)[1]
-                # save file to disk
-                pil_img = Image.open(img_path)
-                # add index & underlines to text for regex
-                pil_img.save(f"{app.config['RESULTS_FOLDER']}/{index}_{text}{img_extension}")
-                print(f"worte image to {app.config['RESULTS_FOLDER']}/{index}_{text}{img_extension}")
+                # copy file with a new name!
+                shutil.copy(img_path, f"{RESULTS_FOLDER}/{index}_{text}{img_extension}")
+                print(f"[INFO] Wrote image to {RESULTS_FOLDER}/{index}_{text}{img_extension}")
                 # update index to read in cursor
                 cursor.increase_index()
         # go to next image if skip is entered
-        if request.form['action'] == "Skip":
+        elif request.form['action'] == "Skip":
             cursor.increase_index()
         # jump to a specific index, choose from indexes under 'images' key in cursor.json
-        if request.form['action'] == "Jump":
+        elif request.form['action'] == "Jump":
             try:
                 jump_index = int(request.form['jump_index'])
                 cursor.set_index(jump_index)
@@ -66,4 +68,4 @@ def action():
 
 
 if __name__ == '__main__':
-    launch_gui(app, port=5000)
+    launch_gui(app, port=PORT)
