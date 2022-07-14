b33f6b50ac30d0d7701a50819efeffb95275684e
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-07-14 16:18:36 +0430

    Update cursor saving and writing protocols

--- cursor.py
+++ cursor.py
@@ -1,15 +1,17 @@
 import os
 from os.path import isfile, join
-import json
+from deep_utils import dump_json, load_json
 
 
 class Cursor:
-    def __init__(self, path, data_dir, cursor_path):
-        self.cursor_path = cursor_path
+    def __init__(self, data_dir):
         self.data_dir = data_dir
-        self.path = path
+        if not os.path.isdir(self.data_dir):
+            raise NotADirectoryError(f"dataset: {self.data_dir} is not a directory!")
+        dir_, name = os.path.split(data_dir)
+        self.cursor_path = join(dir_, f"ocr-labeling-cursor-{name}-cursor.json")
         self._cursor_dict = self._initialize()
-    
+
     def __str__(self) -> str:
         return str(self._cursor_dict)
 
@@ -18,24 +20,17 @@ class Cursor:
 
     def __len__(self):
         return len(self._cursor_dict['images'])
-    
+
     def __setitem__(self, key, data):
         self._cursor_dict[key] = data
-
-    def _dump(self):
-        """
-        Writes cursor dict object to `cursor.json` file.
-        """
-        with open(self.path, 'w') as f:
-            json.dump(self._cursor_dict, f)
 
     def _initialize(self):
         """
         Creates or loads the `cursor.json` file.
         """
         # Fetch or create cursor file
-        if not os.path.exists(self.path):
-            cursor = {'file_index_to_read': 1, 'images': {}}
+        if not os.path.exists(self.cursor_path):
+            cursor = {'file_index_to_read': 1, 'images': {}, "data_dir": self.data_dir}
             img_files = os.listdir(self.data_dir)
             index = 1
             for img_file in img_files:
@@ -43,22 +38,10 @@ class Cursor:
                     cursor['images'][str(index)] = img_file
                 index += 1
 
-            with open(self.cursor_path, 'w') as f:
-                json.dump(cursor, f)
+            dump_json(self.cursor_path, cursor)
         # Open cursor file
-        with open(self.cursor_path) as f:
-            cursor = json.load(f)
+        cursor = load_json(self.cursor_path)
         return cursor
-
-    def reload_file(self):
-        """
-        Reload cursor from disk. Main usage is when a parameter is changed
-        by hand in `cursor.json` file. It's also called every time `index.html` is rendered.
-        """
-        # Open cursor file
-        with open(self.cursor_path) as f:
-            cursor = json.load(f)
-        self._cursor_dict = cursor
 
     def set_index(self, index: int):
         """
@@ -68,8 +51,11 @@ class Cursor:
         if index > len(self):
             index = len(self)
         self._cursor_dict['file_index_to_read'] = index
-        self._dump()
+        dump_json(self.cursor_path, self._cursor_dict)
 
     def increase_index(self):
         self.set_index(self._cursor_dict['file_index_to_read'] + 1)
-        self._dump()
+        dump_json(self.cursor_path, self._cursor_dict)
+
+    def reload_file(self):
+        self._cursor_dict = load_json(self.cursor_path)
